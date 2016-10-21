a0d5b7b9d47d9ae06eef56af19a846678002f8b6
iamthedival <bandiv2011@gmail.com>
2016-10-21 06:21:18 -0400

    Scrabble GUI

--- src/scabbleGame.java
+++ src/scabbleGame.java
@@ -2,4 +2,36 @@
  * Created by div on 10/20/2016.
  */
 public class scabbleGame {
+
+    public void getWords(){
+
+    }
+
+
+    public void grabTiles(){
+
+    }
+
+    public void replaceTile(){
+
+    }
+
+    int scoreWord(){
+        int score = 0;
+
+        return score;
+    }
+
+    public int checkHand(){
+        int check = 0;
+        return check;
+    }
+
+    public boolean checkWord(String word){
+        Boolean exists = false;
+
+
+        return exists;
+    }
+
 }
